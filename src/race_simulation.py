14986123335d32873ea47574b1b7ff74062210dc
Kazai PC Nounou <maximeanjy@gmail.com>
2025-10-22 23:16:07 +0200

    initialisation du fichier de simulation de course

---
+++ src/race_simulation.py
@@ -0,0 +1,36 @@
+"""
+    This file contains the fitness function
+"""
+import math
+from src.runner import Runner
+
+AIR_RESISTANCE_FACTOR = 0.005 
+GRAVITY_FACTOR = 9.81 * 0.02  
+POWER_TO_VELOCITY_RATIO = 0.1
+
+##############################################################
+#                                                            #
+#                 ---- AUX FUNCTIONS ----                    #
+#                                                            #
+##############################################################
+
+def calculate_power(runner):
+    raise Exception("TODO")
+
+def calculate_biomechanics_efficiency(runner):
+    raise Exception("TODO")
+
+
+
+
+
+
+##############################################################
+#                                                            #
+#             ---- MAIN FITNESS FUNCTION ----                #   
+#                                                            #
+##############################################################
+
+
+def calculate_fitness(runner: Runner):
+    raise Exception("TODO")
